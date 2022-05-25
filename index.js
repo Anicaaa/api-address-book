be8c27fe935048a14aa67bf25399746e19183bdd
Anica Rahman <anicarahman2009@yahoo.it>
2022-05-25 17:36:59 +0100

    Complete

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const port = 3030;
 
 const contacts = require("./data/contacts");
 
+const meetings = require("./data/meetings");
+
 //Tell express we want to use the morgan library
 app.use(morgan("dev"));
 //Tell express we want to use the cors library
@@ -27,6 +29,36 @@ app.get("/", (req, res) => {
 app.get("/contacts", (req, res) => {
   res.json({ contacts: contacts });
 });
+
+app.get("/contacts/:id", (req, res) => {
+  //0. extract id from the path
+  //1. search the contacts array
+  //2. find the contact
+  //console.log("params", req.params);
+  //console.log("id", req.params.id);
+  const contact = contacts.find((item) => item.id === +req.params.id);
+  res.json({ contact: contact });
+});
+
+app.get("/contacts/:id/meetings", (req, res) => {
+  const meetingsList = meetings.filter(
+    (item) => item.contactId === req.params.id
+  );
+  console.log("meetings", meetingsList);
+  res.json({ meetings: meetingsList });
+});
+
+/* app.get("/contacts/:id/meetings", (req, res) => {
+  console.log("got meetings");
+  const id = req.params.id;
+  console.log(id);
+  const meetings = meetingsOne.filter((contact) => {
+    console.log();
+    return contact.contactId === id;
+  });
+  console.log("meeting:", { meetings });
+  res.json({ meetings });
+}); */
 
 //Start up our server
 app.listen(port, () => {
